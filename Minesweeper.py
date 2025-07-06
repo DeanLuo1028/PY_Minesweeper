b68fb031d9d81eb0dad784d8f3476406eeb17a74
DeanLuo <phjh1090226@gmail.com>
2025-07-07 01:27:52 +0800

    新增改變格子背景顏色和字體顏色的功能，還有踩到地雷時可以回到上一步 modified README

--- Minesweeper.py
+++ Minesweeper.py
@@ -1,16 +1,14 @@
 import tkinter as tk
 from tkinter import messagebox
 import random
-import numpy as np
+from tkinter import ttk
+from numpy import empty
 from sys import exit
-
-
-TILE_BG = "pink"  # 按鈕背景色
-TILE_FG = "white"  # 按鈕文字顏色
+# -*- coding: utf-8 -*-
 
 class Land(tk.Button):
-    def __init__(self, game, x, y, font_size=12):
-        super().__init__(game.panel, width=2, height=1, font=("Arial", font_size), bg=TILE_BG, fg=TILE_FG, state="normal")
+    def __init__(self, game, x, y, font_size=12, land_bg="white", land_fg="black"):
+        super().__init__(game.panel, width=2, height=1, font=("Arial", font_size), bg=land_bg, fg=land_fg, state="normal")
         self.game = game
         self.x = x
         self.y = y
@@ -43,29 +41,27 @@ class Land(tk.Button):
         count = 0
         for i in range(self.x - 1, self.x + 2):
             for j in range(self.y - 1, self.y + 2):
-                if not (i == self.x and j == self.y) and self.isMineAt(i, j):
+                if not (i == self.x and j == self.y) and self.game.isMineAt(i, j):
                     count += 1
         return count
 
-    def isMineAt(self, x, y):
-        if 0 <= x < self.game.x_range and 0 <= y < self.game.y_range:
-            return self.game.lands[x][y].isMine
-        return False
+    
 
     def clickLand(self):
-        if not self.game.gameFinished: # 如果遊戲還沒結束才能點擊格子
-            #print("遊戲已經結束，不能點擊格子！")
+        if self.game.gameFinished: # 如果遊戲還沒結束才能點擊格子
+            print("遊戲已經結束，不能點擊格子！")
             return
         if not self.isClicked and not self.isFlagged: # 還沒被翻開的格子且沒插旗才能點擊
-            #print(f"點擊了({self.x},{self.y})")
+            if self.isMine:
+                self.game.click_on_mine(self.x, self.y)  # 點到地雷了
+                return
+            print(f"點擊了({self.x},{self.y})")
             self.isClicked = True
             self.config(bg="gray", fg="white", text=self.display)
             self.game.remainingLands -= 1
             #print(f"剩餘需翻開的格子數為 {self.game.remainingLands}")
-            if self.isMine:
-                self.game.gameover()
-            elif self.game.isFirstClick:
-                self.game.start(self.x, self.y)  # 開始遊戲，裝地雷
+            if self.game.isFirstClick:
+                self.game.laying_mines(self.x, self.y)  # 開始遊戲，裝地雷 # TODO
                 self.game.isFirstClick = False
                 self.config(bg="gray", fg="white", text=self.display)
                 self.firstClick(self.x, self.y)
@@ -163,15 +159,17 @@ class Land(tk.Button):
 
 
 class Minesweeper:
-    def __init__(self, x_range, y_range, minesNumber):
+    def __init__(self, x_range, y_range, mines_number, times_of_can_back=0):
         setting_window.destroy()  # 關閉開始視窗
         self.gameFinished = False
         self.x_range = x_range
         self.y_range = y_range
-        self.minesNumber = minesNumber
+        self.mines_number = mines_number
         self.isFirstClick = True
-        self.remainingLands = x_range * y_range - minesNumber
-        self.lands = np.empty((x_range, y_range), dtype=object)  # 建立二維陣列儲存格子物件
+        self.remainingLands = x_range * y_range - mines_number
+        self.times_of_can_back = times_of_can_back # 用來計算可以回到上一個狀態的次數
+        self.land_bg , self.land_fg = "white", "black"  # 按鈕背景色與文字顏色
+        self.lands = empty((x_range, y_range), dtype=object)  # 建立二維陣列儲存格子物件
         self.time = 0  # 初始化時間屬性
         
         # 設定字體大小與視窗大小
@@ -201,6 +199,8 @@ class Minesweeper:
         self.timer = tk.Label(self.root, textvariable=self.timer_var, bg="pink", fg="black")
         self.timer.grid(row=0, column=2)
 
+        self.color_picker = tk.Button(self.root, text="設定格子背景色與文字顏色", command=self.setLandBgFg)
+        self.color_picker.grid(row=2, column=0)
         # 開始遊戲
         self.root.after(1000, self.updateTimer)  # 每秒更新一次計時器
         self.root.mainloop()
@@ -211,8 +211,8 @@ class Minesweeper:
             self.timer_var.set(f"{self.time}秒")  # 更新顯示的時間
             self.root.after(1000, self.updateTimer)  # 每秒更新一次計時器
 
-    def start(self, x, y): # 傳入第一個點擊的格子的座標，確保第一次點擊的格子不是地雷
-        for _ in range(self.minesNumber):
+    def laying_mines(self, x, y): # 傳入第一個點擊的格子的座標，確保第一次點擊的格子不是地雷
+        for _ in range(self.mines_number):
             while True:
                 randomX = random.randint(0, self.x_range - 1)
                 randomY = random.randint(0, self.y_range - 1)
@@ -235,13 +235,80 @@ class Minesweeper:
             for j in range(self.y_range):
                 self.lands[i][j].setDisplay() # 設定格子的顯示文字
 
+    def isMineAt(self, x, y):
+        if 0 <= x < self.x_range and 0 <= y < self.y_range:
+            return self.lands[x][y].isMine
+        return False
+    
+    def setLandBgFg(self):
+        root = tk.Tk()
+        root.geometry("300x150")
+        root.title("設定格子背景色與文字顏色")
+        bg_var = tk.StringVar()
+        bg_var.set("white")  # 預設為白色
+        bg_label = tk.Label(root, text="格子背景色:")
+        bg_color_label = tk.Label(root, textvariable=bg_var, fg = bg_var.get())
+        colors = ["white", "black", "gray", "red", "green", "blue", "yellow", "pink", "orange"]
+        bg_box = ttk.Combobox(root, values=colors)
+        bg_box.set("white")  # 預設為白色
+        def setLandBg(color):
+            self.land_bg = color
+            bg_var.set(bg_box.get())
+            bg_color_label.config(fg=bg_var.get())  # 更新顏色標籤的顏色
+            for i in range(self.x_range):
+                for j in range(self.y_range):
+                    if not self.lands[i][j].isClicked:  # 如果格子已經被點擊過，則不改變背景色
+                        self.lands[i][j].config(bg=self.land_bg)  # 更新格子的背景顏色
+        bg_sure_btn = tk.Button(root, text="確定", command=lambda: setLandBg(bg_box.get()))
+        
+        fg_var = tk.StringVar()
+        fg_var.set("black")  # 預設為黑色
+        fg_label = tk.Label(root, text="格子文字顏色:")
+        fg_color_label = tk.Label(root, textvariable=fg_var, fg=fg_var.get())
+        fg_box = ttk.Combobox(root, values=colors)
+        fg_box.set("black")  # 預設為黑色
+        def setLandFg(color):
+            self.land_fg = color
+            fg_var.set(fg_box.get())
+            fg_color_label.config(fg=fg_var.get())  # 更新顏色標籤的顏色
+            for i in range(self.x_range):
+                for j in range(self.y_range):
+                    if not self.lands[i][j].isClicked:  # 如果格子已經被點擊過，則不改變文字顏色
+                        self.lands[i][j].config(fg=self.land_fg)  # 更新格子的文字顏色
+        fg_sure_btn = tk.Button(root, text="確定", command=lambda: setLandFg(fg_box.get()))
+        
+        bg_label.grid(row=0, column=0)
+        bg_color_label.grid(row=0, column=1)
+        bg_box.grid(row=0, column=2)
+        bg_sure_btn.grid(row=0, column=3)
+        fg_label.grid(row=1, column=0)
+        fg_color_label.grid(row=1, column=1)
+        fg_box.grid(row=1, column=2)
+        fg_sure_btn.grid(row=1, column=3)
+    
+    def click_on_mine(self, x, y):
+        if self.times_of_can_back > 0:
+            root = tk.Tk()
+            root.title("踩到地雷了!")
+            root.geometry("300x150")
+            label = tk.Label(root, text=f"你踩到了地雷({x},{y})，但你還有{self.times_of_can_back}次機會可以回到上一步")
+            label.pack()
+            btn = tk.Button(root, text="回到上一步", command=root.destroy)
+            btn.pack()
+            give_up_btn = tk.Button(root, text="放棄", command=self.gameover)
+            give_up_btn.pack()
+            self.times_of_can_back -= 1
+            root.mainloop()
+        else:
+            self.gameover()
+    
     def gameover(self):
         self.gameFinished = True
         messagebox.showinfo("遊戲結束!", f"你踩到了地雷，遊戲結束！\n共耗時{self.time}秒")
         for i in range(self.x_range):
             for j in range(self.y_range):
-                t = self.lands[i][j].endState() # 設定格子的顏色
-        self.ask_restart()  # 新增這行
+                self.lands[i][j].endState() # 設定格子的顏色
+        self.ask_restart()
 
     def win(self):
         self.gameFinished = True
@@ -249,42 +316,47 @@ class Minesweeper:
         for i in range(self.x_range):
             for j in range(self.y_range):
                 self.lands[i][j].endState() # 設定格子的顏色
-        self.ask_restart()  # 新增這行
+        self.ask_restart()
     
     def ask_restart(self):
         answer = messagebox.askyesno("再玩一次", "要再玩一次嗎？")
         self.root.destroy()
         if answer:
-            main()
+            game_setting()
         else:
             exit(0)
                 
 
 
 def custom():
-    def start(x, y, mines):
+    def start(x, y, mines, times_of_can_back):
         x, y, mines = x.strip(), y.strip(), mines.strip()
-        if (not x.isdigit()) or (not y.isdigit()) or (not mines.isdigit()):
-            messagebox.showerror("輸入錯誤!", "請輸入正確的遊戲大小及地雷數")
+        times_of_can_back = times_of_can_back.strip()
+        if (not x.isdigit()) or (not y.isdigit()) or (not mines.isdigit()) or (not times_of_can_back.isdigit()):
+            messagebox.showerror("輸入錯誤!", "請輸入正確的遊戲大小、地雷數及可回到上一個狀態的次數")
             x_entry.delete(0, tk.END)
             y_entry.delete(0, tk.END)
             mines_entry.delete(0, tk.END)
             return
-        x, y, mines = int(x), int(y), int(mines)
+        x, y, mines, times_of_can_back = int(x), int(y), int(mines), int(times_of_can_back)
         if x < 3 or y < 3 or mines < 1:
             messagebox.showerror("遊戲大小或地雷數過小!", "遊戲大小不能小於3x3，地雷數不能小於1")
             x_entry.delete(0, tk.END)
             y_entry.delete(0, tk.END)
             mines_entry.delete(0, tk.END)
             return
-        elif x > 30 or y > 20:
-            messagebox.showerror("遊戲大小過大!", "遊戲大小不能超過30x20")
+        elif x > 49 or y > 23:
+            messagebox.showerror("遊戲大小過大!", "遊戲大小不能超過49x23")
             x_entry.delete(0, tk.END)
             y_entry.delete(0, tk.END)
             return
         elif mines > x * y:
             messagebox.showerror("地雷數過多!", "地雷數不能超過格子數")
             mines_entry.delete(0, tk.END)
+            return
+        elif times_of_can_back < 0:
+            messagebox.showerror("可回到上一個狀態的次數錯誤!", "可回到上一個狀態的次數不能小於0")
+            times_of_can_back_entry.delete(0, tk.END)
             return
         root.destroy() # 關閉開始畫面，這行超重要！因為沒有它，計時器就會壞掉
         Minesweeper(x, y, mines)
@@ -306,23 +378,28 @@ def custom():
     text_mines_label.grid(row=3, column=0)
     mines_entry = tk.Entry(root)
     mines_entry.grid(row=3, column=1)
-    start_button = tk.Button(root, text="開始遊戲", command=lambda: start(x_entry.get(), y_entry.get(), mines_entry.get()))
-    start_button.grid(row=4, column=0, columnspan=2)
+    times_of_can_back_label = tk.Label(root, text="回到上一個狀態的次數(0表示不能):")
+    times_of_can_back_label.grid(row=4, column=0)
+    times_of_can_back_entry = tk.Entry(root)
+    times_of_can_back_entry.grid(row=4, column=1)
+    start_button = tk.Button(root, text="開始遊戲", command=lambda: \
+        start(x_entry.get(), y_entry.get(), mines_entry.get(), times_of_can_back_entry.get()))
+    start_button.grid(row=5, column=0, columnspan=2)
     root.mainloop()
 
 
-def main():
+def game_setting():
     global setting_window
     setting_window = tk.Tk()
     setting_window.title("歡迎來到踩地雷!")
     setting_window.geometry("300x150")
     text_label = tk.Label(setting_window, text="請選擇遊戲模式:")
     text_label.pack()
-    beginner_button = tk.Button(setting_window, text="初級(8x8)", command=lambda: Minesweeper(8, 8, 10))
+    beginner_button = tk.Button(setting_window, text="初級(8x8)", command=lambda: Minesweeper(8, 8, 10, 2))
     beginner_button.pack()
-    intermediate_button = tk.Button(setting_window, text="中級(16x16)", command=lambda: Minesweeper(16, 16, 40))
+    intermediate_button = tk.Button(setting_window, text="中級(16x16)", command=lambda: Minesweeper(16, 16, 40, 1))
     intermediate_button.pack()
-    expert_button = tk.Button(setting_window, text="專家(24x24)", command=lambda: Minesweeper(24, 24, 99))
+    expert_button = tk.Button(setting_window, text="專家(24x20)", command=lambda: Minesweeper(24, 20, 80))
     expert_button.pack()
     custom_button = tk.Button(setting_window, text="自定義", command=custom)
     custom_button.pack()
@@ -330,4 +407,4 @@ def main():
     
 
 if __name__ == "__main__":
-    main()
+    game_setting()  # 啟動遊戲設定界面
